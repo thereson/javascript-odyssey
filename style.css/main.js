8631836737a16f97ff14093a30900431ee3b618e
thereson <gerardokolie09@gmail.com>
2022-03-26 16:05:24 -0700

    guessing game

---
+++ style.css/main.js
@@ -0,0 +1,7 @@
+const score = document.querySelector(".lit");
+const highsore = document.querySelector(".highscore");
+const guess = document.querySelector(".guess");
+const input = document.querySelector(".input");
+const secretNum = Math.trunc(Math.random()) * 20 + 1;
+
+console.log(secretNum);
